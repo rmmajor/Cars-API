a4e4164b48e807de360159f2895b471e71b5c621
rmmajor <roman.yu.major@gmail.com>
2023-01-23 14:41:01 +0200

    First ugly tests just to learn

--- brand/tests.py
+++ brand/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from rest_framework import test, status
+from django.urls import reverse
+from .views import BrandViewSet
 
-# Create your tests here.
+
+# Tests for Brand endpoints.
+class TestBrandInstanceCRUD(test.APITestCase):
+
+    def test_testing(self):
+        response = self.client.get(reverse('brand-list', current_app='brand'))
+        print(response)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+import pytest
+from rest_framework import status
+from rest_framework.exceptions import ErrorDetail
+
+registration_credentials = dict(
+        username="testUserName",
+        email="testUserName@mail.test",
+        password="testUserName",
+        confirmed_password="testUserName"
+    )
+
+registration_url = 'http://127.0.0.1:8000/user/create/'
+
 
 # Create your tests here.
+@pytest.mark.django_db
+def test_register_user_success(client):
+
+    response = client.post(registration_url, registration_credentials)
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+def test_register_user_passwords_mismatch(client):
+
+    bad_credentials = registration_credentials
+    bad_credentials['confirmed_password'] = 'mismatched pass'
+    response = client.post(registration_url, bad_credentials)
+    expected_data = {'non_field_errors': [ErrorDetail(string='Entered passwords mismatched', code='invalid')]}
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.data == expected_data
+
+
+@pytest.mark.django_db
+def test_register_user_without_confirmed_password(client):
+    bad_credentials = registration_credentials
+    bad_credentials.pop('confirmed_password')
+    response = client.post(registration_url, bad_credentials)
+    # expected_data = {'non_field_errors': [ErrorDetail(string='Entered passwords mismatched', code='invalid')]}
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    # assert response.data == expected_data
+
+
