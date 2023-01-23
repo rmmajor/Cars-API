f78085c8ec09ad76fb29fe9ad384c304cff5d3be
rmmajor <roman.yu.major@gmail.com>
2023-01-23 18:35:35 +0200

    tests_user_register.py refactor

--- user/tests/tests_user_register.py
+++ user/tests/tests_user_register.py
@@ -1,64 +1,81 @@
 import pytest
 from rest_framework import status
 from rest_framework.exceptions import ErrorDetail
-from .fixtures import *
 
 
 # Create your tests here.
-@pytest.mark.django_db  # <- will create a mock database for you to test
-def test_register_user_success(client, good_registration_credentials, registration_url):
-
-    response = client.post(registration_url, good_registration_credentials)
-    assert response.status_code == status.HTTP_201_CREATED
-    assert response.data['access']
-    assert response.data['refresh']
-
-
 @pytest.mark.django_db
-def test_register_user_passwords_mismatch(client, good_registration_credentials, registration_url):
+class TestRegisterUser:
 
-    bad_registration_credentials = good_registration_credentials
-    bad_registration_credentials['confirmed_password'] = 'mismatched pass'
-    response = client.post(registration_url, bad_registration_credentials)
-    expected_response = {'non_field_errors': [ErrorDetail(string='Entered passwords mismatched', code='invalid')]}
+    @pytest.fixture(autouse=True)
+    def initialize(self):
+        self.good_registration_credentials = dict(
+                username="testUserName",
+                email="testUserName@mail.test",
+                password="testUserName",
+                confirmed_password="testUserName")
 
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.data == expected_response
+        self.registration_url = 'http://127.0.0.1:8000/user/create/'
 
+    def test_register_user_success(self, client):
 
-@pytest.mark.django_db
-def test_register_user_without_confirmed_password(client, good_registration_credentials, registration_url):
-    bad_credentials = good_registration_credentials
-    bad_credentials.pop('confirmed_password')
-    response = client.post(registration_url, bad_credentials)
-    expected_response = {"confirmed_password": [
-        "This field is required."
-    ]}
+        response = client.post(self.registration_url, self.good_registration_credentials)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['access']
+        assert response.data['refresh']
 
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.data == expected_response
+    def test_register_user_passwords_mismatch(self, client):
 
+        bad_registration_credentials = self.good_registration_credentials
+        bad_registration_credentials['confirmed_password'] = 'mismatched pass'
+        response = client.post(self.registration_url, bad_registration_credentials)
+        expected_response = {'non_field_errors': [ErrorDetail(string='Entered passwords mismatched', code='invalid')]}
 
-@pytest.mark.django_db
-def test_register_user_without_bad_password(client, good_registration_credentials, registration_url):
-    """
-    Tests password mask (bad caracters, less than 8 caracters, more than 28, etc)
-    """
-    # todo: add Parametrization later
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data == expected_response
 
-    bad_credentials = good_registration_credentials
-    bad_credentials['password'] = "1"
-    bad_credentials['confirmed_password'] = "1"
-    response = client.post(registration_url, bad_credentials)
+    def test_register_user_without_confirmed_password(self, client):
+        bad_credentials = self.good_registration_credentials
+        bad_credentials.pop('confirmed_password')
+        response = client.post(self.registration_url, bad_credentials)
+        expected_response = {"confirmed_password": [
+            "This field is required."
+        ]}
 
-    print(response.status_code)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data == expected_response
 
+    def test_register_user_without_bad_password(self, client):
+        """
+        Tests password mask (bad caracters, less than 8 caracters, more than 28, etc)
+        """
+        # todo: add Parametrization later
 
-@pytest.mark.django_db
-def test_register_user_bad_username(client, good_registration_credentials, registration_url):
-    pass
+        bad_credentials = self.good_registration_credentials
+        bad_credentials['password'] = "1"
+        bad_credentials['confirmed_password'] = "1"
+        response = client.post(self.registration_url, bad_credentials)
 
+        print(response.status_code)
 
-@pytest.mark.django_db
-def test_register_user_bad_email(client, good_registration_credentials, registration_url):
-    pass
+    def test_register_user_too_long_username(self, client):
+        bad_credentials = self.good_registration_credentials
+        long_username = "FvM4sgubtxXwelCTl1LHrb3lUrpGIpnBgDVQxnIfcK6EUyXYC" \
+                        "b0etf6oNsUyMsdMYPomre8JILcbDQP6s8q4l7BVR8EFYXclbtI" \
+                        "LfAVQGDPDsTZGcgafHynWWT4tuZBRcgy4vqymf67tb61Flzp5YG3"
+
+        bad_credentials['username'] = long_username
+        response = client.post(self.registration_url, bad_credentials)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data == {'username': [ErrorDetail(string='Ensure this field has no more than 150 characters.',
+                                                          code='max_length')]}
+
+    def test_register_user_bad_email(self, client):
+        bad_credentials = self.good_registration_credentials
+        bad_email = "not email"
+
+        bad_credentials['email'] = bad_email
+        response = client.post(self.registration_url, bad_credentials)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data == {'email': [ErrorDetail(string='Enter a valid email address.', code='invalid')]}
